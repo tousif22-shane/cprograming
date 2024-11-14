ff618041440221029b7a5071e1380b3564b07808
tousif22-shane <rcoetousif@eng.rizvi.edu.in>
2024-11-14 11:36:01 +0530

    Add files via upload

---
+++ TOUSIF2.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+ int main()
+{
+   printf("\nUsing do-While loop:\n");
+ int k=1;
+ do
+ {
+    printf("%d\n",k);
+    k++;
+
+
+ }while(k<=5);
+
+}
---
+++ tousif3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+ int main()
+{
+   printf("Using loop:\n");
+   for(int k=1;k<=5;k++)
+
+
+ {
+    printf("%d\n",k);
+
+
+}
+
+}
